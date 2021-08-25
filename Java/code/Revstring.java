9d81ac386f21b1f198fe980d19f69ae9db441154
Mananjay Roy <roymananjay@gmail.com>
2021-08-26 02:02:01 +0530

    update

---
+++ Java/code/Revstring.java
@@ -0,0 +1,14 @@
+/*
+ * How to reverse a String in java? Can you write a program without using any java inbuilt methods?
+ */
+import java.util.*;
+class ReverseArray{
+public static void main(String[] args)
+{
+	Scanner sc = new Scanner(System.in);
+	String scr= sc.nextLine();
+	
+	
+}	
+	
+}
