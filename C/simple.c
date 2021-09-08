268a5571ebd819464c76b1983b4cbfb342278254
Mananjay Roy <roymananjay@gmail.com>
2021-09-08 15:04:25 +0530

    update

---
+++ C/simple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j;
+	printf("Enter the value of i \n");
+	scanf("%d",&i);
+	scanf("%d",&j);
+	
+	printf("The values are ");
+	printf("%d %d",i,j);
+	//printf("%d",i);
+	
+}
---
+++ C/tertiary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a&b&c);
+	
+	if(a>b?(if(a>c)?printf("%d",a):printf("%d",c)):)
+	
+}
