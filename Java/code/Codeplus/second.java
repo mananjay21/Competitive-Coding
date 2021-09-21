273316ce8cf90401565b3583b9d1961807b04ab6
Mananjay Roy <roymananjay@gmail.com>
2021-09-22 00:33:45 +0530

    update

--- Java/code/Codeplus/second.java
+++ Java/code/Codeplus/second.java
@@ -1,6 +1,15 @@
+
 public class second {
     public static void main(String[] args)
     {
         System.out.println("hello Mananjay");
+        int num1=2;
+        int num2=3;
+        int num3=4;
+        short num4=10;
+        System.out.println(num4);
+        boolean ma =true;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
     }   
 }
