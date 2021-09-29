d28e0ca166ea2eb035ae822af4bf61826f030ac6
Mananjay Roy <roymananjay@gmail.com>
2021-09-30 00:33:18 +0530

    update

--- Java/CWH/lowecase.java
+++ Java/CWH/lowecase.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
-public class lowecase {
+ class lowecase {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in());
         String name=sc.next();
         String name1=name.toUpperCase();
         System.out.println(name1);
