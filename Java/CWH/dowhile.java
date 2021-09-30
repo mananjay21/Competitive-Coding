5a5c7e15213d5fab76e9782d4549cf82972ac29e
Mananjay Roy <roymananjay@gmail.com>
2021-10-01 02:19:54 +0530

    update

---
+++ Java/CWH/dowhile.java
@@ -0,0 +1,10 @@
+class dowhile
+{
+    public static void main(String[] args) {
+        int a=10;
+        do{
+            System.out.println(a);
+            a++;
+        }while(a<45);
+    }
+}
--- Java/CWH/lowecase.java
+++ Java/CWH/lowecase.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
  class lowecase {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in());
-        String name=sc.next();
+        ///Scanner sc = new Scanner(System.in());
+        //String name=sc.next();
         //int n = sc.nxtInt();
        
-        String name1=name.toUpperCase();
-        System.out.println(name1);
+        //String name1=name.toUpperCase();
+        //System.out.println(name1);
     }
     
 }
