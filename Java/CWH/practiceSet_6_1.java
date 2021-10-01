d69d7c67f0534c8ad12e43f6a9f6fb4db4508490
Mananjay Roy <roymananjay@gmail.com>
2021-10-02 00:49:18 +0530

    update

---
+++ Java/CWH/practiceSet_6_1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class practiceSet_2_1
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("Practice problem number 2_1");
+		System.out.println("Addition of float number ");
+		Scanner sc= new Scanner(System.in);
+		int n=5;
+		float sum=0;
+		float arr[]= new float[n];
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextFloat();	
+		}
+		//for calculating sum of numbers....
+		for(int i=0;i<arr.length;i++)
+		{
+			sum=sum+arr[i];
+		}
+		System.out.println("Sum of all float number "+sum);
+	}
+}
+
