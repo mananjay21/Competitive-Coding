ec4e3957fa9b6ddcaa5e4aad618bdd7a440ea4fe
Mananjay Roy <roymananjay@gmail.com>
2021-10-06 03:37:03 +0530

    update

--- Java/CWH/practiceSet3_4.java
+++ Java/CWH/practiceSet3_4.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 class practiceSet3_4
 {
-	static int avg(float a,float b)
+	static int avg(double a,double b)
 	{
 	
-	return float(a+b)/2;	
+	double m =(a+b)/2f;	
+	System.out.println(m);
+	return m;
 	}
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Program to  pass value in argument and calculate average ");
 		System.out.println("Print the value of A");
-		float a= sc.nextFloat();
+		double a= sc.nextFloat();
 		System.out.println("Print the value of B");
-		float b= sc.nextFloat();
-		System.out.println("Average of number are :" +avg(a,b));
+		double b= sc.nextFloat();
+		//System.out.println("Average of number are :" +avg(a,b));
+		avg(a,b);
 		
 		
 	}
