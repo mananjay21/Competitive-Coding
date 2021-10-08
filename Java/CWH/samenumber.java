fc5df1d9836722c837c8c700ca9435d248fdfa9c
Mananjay Roy <roymananjay@gmail.com>
2021-10-08 23:18:13 +0530

    Update

--- Java/CWH/samenumber.java
+++ Java/CWH/samenumber.java
@@ -1,5 +1,27 @@
-inport java.util.Scanner;
+import java.util.Scanner;
 class samenumber
 {
+	public static void  main(String[] args)
+	{
+		Scanner sc=  new Scanner(System.in);
+		System.out.println("program to calculate number of non simillar number ");
+		System.out.println("Enter the first number ");
+		int a= sc.nextInt();
+		System.out.println("Enter the second number ");
+		int b= sc.nextInt();
+		//calculate same number occurance....
+		int m=a;
+		for(int i=a;i<=b;i++)
+		{
+			while(a>0)
+			{
+				int c=m%10;
+				m=m/10;
+				//if(c==
+			}
+			
+		}
+		
+	}
 
 }
