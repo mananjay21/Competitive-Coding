a092d6611faa1d3538c8c947a4b2d118b46eb3c9
Mananjay Roy <roymananjay@gmail.com>
2021-10-08 18:06:32 +0530

    Update

--- Java/CWH/rotateVal.java
+++ Java/CWH/rotateVal.java
@@ -4,7 +4,20 @@ class rotateVal
 	public static void main(String args[])
 	{
 		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the number of element in array");
 		int n = sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the value in array");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			
+		}
+		System.out.println("Enter total  number of clock wise rotation ");
+		
+		int k  =sc.nextInt();
+		
+		
 		
 		
 	
