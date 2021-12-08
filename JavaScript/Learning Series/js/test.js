b5729500c61083a16b7850542566d27b5174f6ff
Mananjay Roy <roymananjay@gmail.com>
2021-12-08 14:28:17 +0530

    update

---
+++ JavaScript/Learning Series/js/test.js
@@ -0,0 +1,4 @@
+document.write("Hey Learner how are you 😊");// this is a single line comment
+document.write("<br><i>This Quick Evento is an event management platform </i>");/*This 
+is  multiline commnet syntax in javaScript
+*/
